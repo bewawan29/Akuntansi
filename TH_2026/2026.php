0aa3541fc8bf6213c56025067d11dceb2c55159e
bewawan29 <bewawan@gmail.com>
2026-02-28 23:26:06 +0700

    modif

--- TH_2026/2026.php
+++ TH_2026/2026.php
@@ -238,8 +238,11 @@ include_once ("maret/2026hitungmaret.php");
         </div>
         <nav>
             <div class="menu-hide" onclick="muncul()">MENU</div>
+            
             <div class="menu" id="hide">
-
+                <div onclick="tutup()" style="color: white; font-weight: bold; cursor: pointer; margin-bottom: 20px; background: rgba(0,0,0,0.3); padding: 10px; text-align: center;">
+                TUTUP [X]
+                </div>
                 <a href="../logout.php">LOGOUT</a>
                 <a href="../tahun.php">TAHUN</a>
                 <a href="2026type.php">Umum/Pribadi</a>
@@ -256,10 +259,15 @@ include_once ("maret/2026hitungmaret.php");
             <script src="../waktu.js"></script>
             <script src="mundur2026.js?v=<?php echo time(); ?>"></script>
             <script>
-                function muncul(){
-                    // document.getElementById("hide").style.display = "block";
-                    document.getElementById("hide").style.left = "0px";
-                }
+            function muncul() {
+                // Menggeser menu ke posisi 0 (muncul dari kiri)
+                document.getElementById("hide").style.left = "0px";
+            }
+
+            function tutup() {
+                // Mengembalikan menu ke kiri luar layar
+                document.getElementById("hide").style.left = "-100%";
+            }
             </script>
         </nav>
 
